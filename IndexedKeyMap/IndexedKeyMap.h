cc76c79ea25e06ec06fccaba1f48dae85f5a42c6
Alex Gray <alex@mrgray.com>
2014-05-13 01:44:25 -0400

    Initial Commit

---
+++ IndexedKeyMap/IndexedKeyMap.h
@@ -0,0 +1,13 @@
+//
+//  IndexedKeyMap.h
+//  IndexedKeyMap
+//
+//  Created by Alex Gray on 5/13/14.
+//  Copyright (c) 2014 Alex Gray. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IndexedKeyMap : NSObject
+
+@end
---
+++ IndexedKeyMap/IndexedKeyMap.m
@@ -0,0 +1,13 @@
+//
+//  IndexedKeyMap.m
+//  IndexedKeyMap
+//
+//  Created by Alex Gray on 5/13/14.
+//  Copyright (c) 2014 Alex Gray. All rights reserved.
+//
+
+#import "IndexedKeyMap.h"
+
+@implementation IndexedKeyMap
+
+@end
